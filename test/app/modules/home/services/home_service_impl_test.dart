0b5eeb789b37ec83bf1795c0cf43273cf1140ca9
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-01-18 12:35:40 -0300

    Creating test findTotalExpensesByCategories method of HomeServiceImpl close #14

--- test/app/modules/home/services/home_service_impl_test.dart
+++ test/app/modules/home/services/home_service_impl_test.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:onde_gastei_app/app/core/exceptions/failure.dart';
-import 'package:onde_gastei_app/app/core/rest_client/rest_client_exception.dart';
+import 'package:onde_gastei_app/app/models/category_model.dart';
 import 'package:onde_gastei_app/app/models/user_model.dart';
 import 'package:onde_gastei_app/app/modules/home/repositories/home_repository.dart';
 import 'package:onde_gastei_app/app/modules/home/services/home_service.dart';
 import 'package:onde_gastei_app/app/modules/home/services/home_service_impl.dart';
+import 'package:onde_gastei_app/app/modules/home/view_model/total_expenses_categories_view_model.dart';
 
 class MockHomeRepository extends Mock implements HomeRepository {}
 
@@ -33,12 +34,53 @@ void main() {
 
     test('Should throws exception', () async {
       // Arrange
-      when(() => repository.fetchUserData())
-          .thenThrow(Failure());
+      when(() => repository.fetchUserData()).thenThrow(Failure());
       //Act
       final call = service.fetchUserData;
       //Assert
       expect(call, throwsA(isA<Failure>()));
+    });
+  });
+
+  group('Group test findTotalExpensesByCategories', () {
+    test('Should findTotalExpensesByCategories with success', () async {
+      // Arrange
+      const userId = 1;
+      final initialDate = DateTime.now();
+      final finalDate = DateTime.now();
+      final totalExpensesCategoriesExpected = [
+        TotalExpensesCategoriesViewModel(
+          totalValue: 1,
+          category: const CategoryModel(
+            id: 1,
+            description: 'Test',
+            iconCode: 1,
+            colorCode: 1,
+          ),
+        )
+      ];
+      when(() => repository.findTotalExpensesByCategories(
+              userId, initialDate, finalDate))
+          .thenAnswer((_) async => totalExpensesCategoriesExpected);
+      //Act
+      final totalExpensesCategories = await service
+          .findTotalExpensesByCategories(userId, initialDate, finalDate);
+      //Assert
+      expect(totalExpensesCategories, totalExpensesCategoriesExpected);
+    });
+
+    test('Should findTotalExpensesByCategories with success', () async {
+      // Arrange
+      const userId = 1;
+      final initialDate = DateTime.now();
+      final finalDate = DateTime.now();
+      when(() => repository.findTotalExpensesByCategories(any(), any(), any()))
+          .thenThrow(Failure());
+      //Act
+      final call = service.findTotalExpensesByCategories;
+      //Assert
+      expect(
+          () => call(userId, initialDate, finalDate), throwsA(isA<Failure>()));
     });
   });
 }
