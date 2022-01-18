11c72afb9dcc00c968cdd18679fa61c45d62a429
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-01-18 12:47:19 -0300

    Creating test findPercentageByCategories method of HomeServiceImpl close #15

--- test/app/modules/home/services/home_service_impl_test.dart
+++ test/app/modules/home/services/home_service_impl_test.dart
@@ -6,6 +6,7 @@ import 'package:onde_gastei_app/app/models/user_model.dart';
 import 'package:onde_gastei_app/app/modules/home/repositories/home_repository.dart';
 import 'package:onde_gastei_app/app/modules/home/services/home_service.dart';
 import 'package:onde_gastei_app/app/modules/home/services/home_service_impl.dart';
+import 'package:onde_gastei_app/app/modules/home/view_model/percentage_categories_view_model.dart';
 import 'package:onde_gastei_app/app/modules/home/view_model/total_expenses_categories_view_model.dart';
 
 class MockHomeRepository extends Mock implements HomeRepository {}
@@ -78,6 +79,54 @@ void main() {
           .thenThrow(Failure());
       //Act
       final call = service.findTotalExpensesByCategories;
+      //Assert
+      expect(
+          () => call(userId, initialDate, finalDate), throwsA(isA<Failure>()));
+    });
+  });
+
+  group('Group test findPercentageByCategories', () {
+    test('Should findPercentageByCategories with success', () async {
+      // Arrange
+      const userId = 1;
+      final initialDate = DateTime.now();
+      final finalDate = DateTime.now();
+
+      final percentageCategoriesExpected = [
+        const PercentageCategoriesViewModel(
+          value: 1,
+          percentage: 1,
+          category: CategoryModel(
+            id: 1,
+            description: 'Test',
+            iconCode: 1,
+            colorCode: 1,
+          ),
+        )
+      ];
+
+      when(() => repository.findPercentageByCategories(
+              userId, initialDate, finalDate))
+          .thenAnswer((_) async => percentageCategoriesExpected);
+      //Act
+      final percentageCategories = await service.findPercentageByCategories(
+          userId, initialDate, finalDate);
+
+      //Assert
+      expect(percentageCategories, percentageCategoriesExpected);
+    });
+
+    test('Should throws exception', () async {
+      // Arrange
+      const userId = 1;
+      final initialDate = DateTime.now();
+      final finalDate = DateTime.now();
+
+      when(() => repository.findPercentageByCategories(
+          userId, initialDate, finalDate)).thenThrow(Failure());
+      //Act
+      final call = service.findPercentageByCategories;
+
       //Assert
       expect(
           () => call(userId, initialDate, finalDate), throwsA(isA<Failure>()));
