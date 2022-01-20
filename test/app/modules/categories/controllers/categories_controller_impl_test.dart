9da1e6f8fbeae388708a978ccfa66ee0f8b05f40
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-01-19 23:05:07 -0300

    Creating test deleteCategory method of CategoriesControllerImpl close #26

--- test/app/modules/categories/controllers/categories_controller_impl_test.dart
+++ test/app/modules/categories/controllers/categories_controller_impl_test.dart
@@ -12,7 +12,6 @@ import '../../../../core/log/mock_log.dart';
 
 class MockCategoriesServices extends Mock implements CategoriesService {}
 
-
 void main() {
   late CategoriesService service;
   late Log log;
@@ -28,7 +27,7 @@ void main() {
     test('Should register category with success', () async {
       // Arrange
       const categoryModel =
-      CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
+          CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
 
       when(() => service.register(categoryModel)).thenAnswer((_) async => _);
       //Act
@@ -41,7 +40,7 @@ void main() {
     test('Should throws exception', () async {
       // Arrange
       const categoryModel =
-      CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
+          CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
 
       when(() => service.register(categoryModel)).thenThrow(Exception());
       //Act
@@ -58,29 +57,57 @@ void main() {
       // Arrange
       const categoryId = 1;
       const categoryInputModel =
-      CategoryInputModel(description: 'Test', iconCode: 1, colorCode: 1);
+          CategoryInputModel(description: 'Test', iconCode: 1, colorCode: 1);
       when(() => service.updateCategory(categoryId, categoryInputModel))
           .thenAnswer((_) async => _);
       //Act
       await controller.updateCategory(categoryId, categoryInputModel);
 
       //Assert
-      verify(() => service.updateCategory(categoryId, categoryInputModel)).called(1);
+      verify(() => service.updateCategory(categoryId, categoryInputModel))
+          .called(1);
     });
 
     test('Should throws exception', () async {
       // Arrange
       const categoryId = 1;
       const categoryInputModel =
-      CategoryInputModel(description: 'Test', iconCode: 1, colorCode: 1);
-      when(() => service.updateCategory(categoryId, categoryInputModel)).thenThrow(Exception());
+          CategoryInputModel(description: 'Test', iconCode: 1, colorCode: 1);
+      when(() => service.updateCategory(categoryId, categoryInputModel))
+          .thenThrow(Exception());
       //Act
       final call = controller.updateCategory;
 
       //Assert
-      expect(() => call(categoryId, categoryInputModel), throwsA(isA<Failure>()));
-      verify(() => service.updateCategory(categoryId, categoryInputModel)).called(1);
+      expect(
+          () => call(categoryId, categoryInputModel), throwsA(isA<Failure>()));
+      verify(() => service.updateCategory(categoryId, categoryInputModel))
+          .called(1);
+    });
+  });
+
+  group('Group test deleteCategories', () {
+    test('Should delete categories with success', () async {
+      // Arrange
+      when(() => service.deleteCategory(any())).thenAnswer((_) async => _);
+
+      //Act
+      await controller.deleteCategory(1);
+
+      //Assert
+      verify(() => service.deleteCategory(any())).called(1);
     });
 
+    test('Should throws exception', () async {
+      // Arrange
+      when(() => service.deleteCategory(any())).thenThrow(Exception());
+
+      //Act
+      final call = controller.deleteCategory;
+
+      //Assert
+      expect(() => call(1), throwsA(isA<Failure>()));
+      verify(() => service.deleteCategory(any())).called(1);
+    });
   });
 }
