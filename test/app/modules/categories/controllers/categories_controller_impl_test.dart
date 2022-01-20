44b918c5c4b205c4dd08390acf2e3b97e7394291
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-01-19 22:48:10 -0300

    Creating test updateCategory method of CategoriesControllerImpl close #25

--- test/app/modules/categories/controllers/categories_controller_impl_test.dart
+++ test/app/modules/categories/controllers/categories_controller_impl_test.dart
@@ -6,10 +6,12 @@ import 'package:onde_gastei_app/app/models/category_model.dart';
 import 'package:onde_gastei_app/app/modules/categories/controllers/categories_controller.dart';
 import 'package:onde_gastei_app/app/modules/categories/controllers/categories_controller_impl.dart';
 import 'package:onde_gastei_app/app/modules/categories/services/categories_service.dart';
+import 'package:onde_gastei_app/app/modules/categories/view_model/category_input_model.dart';
 
 import '../../../../core/log/mock_log.dart';
 
 class MockCategoriesServices extends Mock implements CategoriesService {}
+
 
 void main() {
   late CategoriesService service;
@@ -26,7 +28,7 @@ void main() {
     test('Should register category with success', () async {
       // Arrange
       const categoryModel =
-          CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
+      CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
 
       when(() => service.register(categoryModel)).thenAnswer((_) async => _);
       //Act
@@ -39,7 +41,7 @@ void main() {
     test('Should throws exception', () async {
       // Arrange
       const categoryModel =
-          CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
+      CategoryModel(id: 1, description: 'Test', iconCode: 1, colorCode: 1);
 
       when(() => service.register(categoryModel)).thenThrow(Exception());
       //Act
@@ -49,5 +51,36 @@ void main() {
       expect(() => call(categoryModel), throwsA(isA<Failure>()));
       verify(() => service.register(categoryModel)).called(1);
     });
+  });
+
+  group('Group test updateCategory', () {
+    test('Should update category with success', () async {
+      // Arrange
+      const categoryId = 1;
+      const categoryInputModel =
+      CategoryInputModel(description: 'Test', iconCode: 1, colorCode: 1);
+      when(() => service.updateCategory(categoryId, categoryInputModel))
+          .thenAnswer((_) async => _);
+      //Act
+      await controller.updateCategory(categoryId, categoryInputModel);
+
+      //Assert
+      verify(() => service.updateCategory(categoryId, categoryInputModel)).called(1);
+    });
+
+    test('Should throws exception', () async {
+      // Arrange
+      const categoryId = 1;
+      const categoryInputModel =
+      CategoryInputModel(description: 'Test', iconCode: 1, colorCode: 1);
+      when(() => service.updateCategory(categoryId, categoryInputModel)).thenThrow(Exception());
+      //Act
+      final call = controller.updateCategory;
+
+      //Assert
+      expect(() => call(categoryId, categoryInputModel), throwsA(isA<Failure>()));
+      verify(() => service.updateCategory(categoryId, categoryInputModel)).called(1);
+    });
+
   });
 }
