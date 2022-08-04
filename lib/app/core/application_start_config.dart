b3be4cce6d0e6e77530ea598fd6d4768d84d91ad
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-08-04 08:26:31 -0300

    Adding firebase crashlitcs in project

--- lib/app/core/application_start_config.dart
+++ lib/app/core/application_start_config.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:onde_gastei_app/app/core/helpers/environments.dart';
 
@@ -8,6 +9,7 @@ class ApplicationStartConfig {
     // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
     await _firebaseConfig();
     await _loadEnvs();
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   }
 
   Future<void> _firebaseConfig() async => Firebase.initializeApp();
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:async';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:onde_gastei_app/app/app.dart';
 import 'package:onde_gastei_app/app/core/application_start_config.dart';
 
 Future<void> main() async {
-  await ApplicationStartConfig().configureApp();
-  runApp(const App());
+  await runZonedGuarded<Future<void>>(
+    () async {
+      await ApplicationStartConfig().configureApp();
+      runApp(const App());
+    },
+    (error, stack) =>
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
+  );
 }
